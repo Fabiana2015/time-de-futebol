941c7f9c147ec5771d653b57e2f04b7627b1523a
Fabiana Amorim <f.fabiana2015@gmail.com>
2023-09-04 11:07:56 -0300

    Iniciando o projeto time de futebol

---
+++ src/test/java/next/school/timedefutebol/TimeDeFutebolApplicationTests.java
@@ -0,0 +1,13 @@
+package next.school.timedefutebol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeDeFutebolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
